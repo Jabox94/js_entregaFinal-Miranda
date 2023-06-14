ca7ea07eaf33da0e1bc95563d20ab945c542d05b
Javier Miranda <Jabox2002@proton.me>
2023-06-14 01:47:40 -0300

    Agregando imports al js

--- js/main.js
+++ js/main.js
@@ -1,66 +1,9 @@
-// Generar html para las cartas de curso
-// JSON de cursos y maqueta de como agregar nuevos cursos
-const dataJSON = `[
-    {
-       "nombre": "Curso de fotografia Fundamentos",
-       "categoria": "fotografia",
-       "precio": 88.9,
-       "id": "CF1"
-    },
-    {
-       "nombre": "Curso de fotografia Intermedio",
-       "categoria": "fotografia",
-       "precio": 120.9,
-       "id": "CF2"
-    },
-    {
-       "nombre": "Curso de fotografia Avanzado",
-       "categoria": "fotografia",
-       "precio": 180.9,
-       "id": "CF3"
-    },
-    {
-       "nombre": "Curso de cocina Fundamentos",
-       "categoria": "cocina",
-       "precio": 88.9,
-       "id": "CC1"
-    },
-    {
-       "nombre": "Curso de cocina Intermedio",
-       "categoria": "cocina",
-       "precio": 120.9,
-       "id": "CC2"
-    },
-    {
-       "nombre": "Curso de cocina Avanzado",
-       "categoria": "cocina",
-       "precio": 180.9,
-       "id": "CC3"
-    },
-    {
-       "nombre": "Curso de JavaScript Fundamentos",
-       "categoria": "programacion",
-       "precio": 88.9,
-       "id": "CJ1"
-    },
-    {
-       "nombre": "Curso de JavaScript Intermedio",
-       "categoria": "programacion",
-       "precio": 120.9,
-       "id": "CJ2"
-    },
-    {
-       "nombre": "Curso de JavaScript Avanzado",
-       "categoria": "programacion",
-       "precio": 180.9,
-       "id": "CJ3"
-    }
- ]
-`;
+// Simulacion de BD con un archivo JS
+import { cursos } from './db.js';
 
 // Convertir JSON a objeto
-const listaCursosJSON = JSON.parse(dataJSON);
-
+const listaCursosJSON = JSON.parse(cursos);
+console.log(listaCursosJSON);
 
 // Identificar con que categoria de curso se trabaja
 function filtrarCategoria() {
